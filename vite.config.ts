d1fd5128dad330df654e15d8171c995925dbdacd
Mujtaba-real <mujtabadrop@gmail.com>
2025-06-14 11:03:15 +0000

    Update project files

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base:"/styleiraq-94/",
   plugins: [
     react(),
     mode === 'development' &&
